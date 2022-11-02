f0e7033be602caa91865ea40363f4f87319165d7
Aryatama <18211338+aryatama045@users.noreply.github.com>
2022-11-02 13:53:16 +0700

    Update view_page.php

--- application/views/home/halaman/view_page.php
+++ application/views/home/halaman/view_page.php
@@ -4,20 +4,20 @@ $card = $url!='page/detail/hadiah-juara'?'card':'';
 $card_body = $url!='page/detail/hadiah-juara'?'card-body':'';
 ?>
 
-<div class="post__header post-header" 
+<!-- <div class="post__header post-header" 
     style="background: hsl(196deg 100% 47%) !important;
     margin-bottom: 20px;
     margin-top: 0px;
     padding-top: 50px;
     padding-bottom: 24px;">
     <h4 class="post-header__title" style="text-align:center;color:white;"><?= $record['judul']; ?></h4>
-</div>
+</div> -->
 
 <div class="container">
     <div class="row">
         <div class="col-12">
-            <div class="block <?= $card ?> post--layout--classic">
-
+            <!-- <div class="block <?= //$card ?> post--layout--classic"> -->
+            <div class="block  post--layout--classic"> 
 
                 <!-- <div class="post__featured"><a href="#"><img src="" alt=""></a></div> -->
 
