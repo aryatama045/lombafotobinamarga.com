7f9b6163304133a534813e6983f3ddfbedd65fd2
Aryatama <18211338+aryatama045@users.noreply.github.com>
2022-11-03 19:49:24 +0700

    Update view_page.php

--- application/views/home/halaman/view_page.php
+++ application/views/home/halaman/view_page.php
@@ -2,6 +2,8 @@
 $url = $this->uri->ruri_string();
 $card = $url!='page/detail/hadiah-juara'?'card':'';
 $card_body = $url!='page/detail/hadiah-juara'?'card-body':'';
+
+// tesx($url);
 ?>
 
 <!-- <div class="post__header post-header" 
@@ -21,7 +23,7 @@ $card_body = $url!='page/detail/hadiah-juara'?'card-body':'';
 
                 <!-- <div class="post__featured"><a href="#"><img src="" alt=""></a></div> -->
 
-                <div class="<?= $card_body ?> post__content typography">
+                <div class=" post__content typography">
 
                     <?= $record['isi_halaman']; ?>
 
@@ -31,4 +33,57 @@ $card_body = $url!='page/detail/hadiah-juara'?'card-body':'';
         </div>
 
     </div>
+
+    <?php if($url == "page/detail/pengiriman-karya"){ ?>
+
+    
+    <div class="row" >
+        <div class="col-lg-12 col-md-6" >
+            <h5 class=" align-center" style="text-align:center; margin-bottom:2em; color:#3961a7;"> JUMLAH PESERTA DAN KARYA YANG DITERIMA</h5>
+            <table class="table table-borderless table-sm pb-3" style="border:2px solid #ffffff;" >
+                <thead>
+                    <tr style="text-align:center;">
+                        <th width='25%'></th>
+                        <th>Minggu 1</th>
+                        <th>Minggu 2</th>
+                        <th>Minggu 3</th>
+                        <th>Minggu 4</th>
+                        <th>Total</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr style="border:2px solid #ffffff; text-align:center;">
+                        <td style="background: #244784;color: white; border:2px solid #ffffff; text-align:center;">
+                            <b> Total Peserta </b></td>
+                        <th style="background: #ffc200;border:2px solid #ffffff; text-align:center;">
+                            0</th>
+                        <th style="background: #ffc200;border:2px solid #ffffff; text-align:center;">
+                            0</th>
+                        <th style="background: #ffc200;border:2px solid #ffffff; text-align:center;">
+                            0</th>
+                        <th style="background: #ffc200;border:2px solid #ffffff; text-align:center;">
+                            0</th>
+                        <th style="background: #ffc200;border:2px solid #ffffff; text-align:center;">
+                            0</th>
+                    </tr>
+                    <tr style="border:2px solid #ffffff; border:2px solid #ffffff; text-align:center;">
+                        <td style="background: #244784;color: white;">
+                            <b> Total Karya </b></td>
+                        <th style="background: #ffc200;border:2px solid #ffffff; text-align:center;">
+                            0</th>
+                        <th style="background: #ffc200;border:2px solid #ffffff; text-align:center;">
+                            0</th>
+                        <th style="background: #ffc200;border:2px solid #ffffff; text-align:center;">
+                            0</th>
+                        <th style="background: #ffc200;border:2px solid #ffffff; text-align:center;">
+                            0</th>
+                        <th style="background: #ffc200;border:2px solid #ffffff; text-align:center;">
+                            0</th>
+                    </tr> 
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    <?php } ?>
 </div>
